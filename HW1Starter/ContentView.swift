2ca5d6cf84d43ff15fb1b0f051d99b90291f7c99
justinwongeecs <jwonghelloworld@berkeley.edu>
2024-09-08 14:42:40 -0700

    Initial Commit

---
+++ HW1Starter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW1Starter
+//
+//  Created by Justin Wong on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW1Starter/HW1StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW1StarterApp.swift
+//  HW1Starter
+//
+//  Created by Justin Wong on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HW1StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
